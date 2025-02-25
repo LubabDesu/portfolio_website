573729a7e0387a1b4d379b302c71311df4ba008f
Lucas Yan <lucasyanyikang88@gmail.com>
2025-02-24 21:03:00 -0800

    Projects page skeleton done

--- src/app/Overview/page.jsx
+++ src/app/Overview/page.jsx
@@ -1,3 +1,43 @@
+import Link from "next/link";
+import "../globals.css";
+
 export default function Overview() {
-    return <div>Overview page</div>;
+    return (
+        <div>
+            <section className="section-header">
+                <h1>Project Overview</h1>
+            </section>
+            <section className="card-container">
+                <MlButton />
+                <SweButton />
+                <MusicButton />
+            </section>
+        </div>
+    );
+}
+
+function MlButton() {
+    return (
+        <Link href="/ml">
+            <button className="buttonCard">Machine Learning Projects</button>
+        </Link>
+    );
+}
+
+function SweButton() {
+    return (
+        <Link href="/swe">
+            <button className="buttonCard">
+                Fullstack / Software Engineering Projects
+            </button>
+        </Link>
+    );
+}
+
+function MusicButton() {
+    return (
+        <Link href="/music">
+            <button className="buttonCard">Music Projects</button>
+        </Link>
+    );
 }
