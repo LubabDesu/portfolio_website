6566a6a83e52fb8b1fdd25adbfece65153eed858
Lucas Yan <lucasyanyikang88@gmail.com>
2025-02-25 11:53:08 -0800

    successfully render pdf resume

--- src/components/PDFResume.jsx
+++ src/components/PDFResume.jsx
@@ -1,3 +1,33 @@
+"use client";
+// import { Worker } from "next/dist/compiled/jest-worker";
+import { Document, Page, pdfjs } from "react-pdf";
+import { useState } from "react";
+
+// Start worker
+pdfjs.GlobalWorkerOptions.workerSrc = "/pdfworker.min.mjs";
 export const PDFResume = () => {
-    return <p>This is the pdf resume</p>;
+    return (
+        <div>
+            <h2>Resume</h2>
+            <Resume />
+        </div>
+    );
 };
+
+function Resume() {
+    const [numPages, setNumPages] = useState(null);
+
+    const onDocumentLoadSuccess = ({ numPages }) => {
+        setNumPages(numPages);
+    };
+    return (
+        <div className="pdf-container">
+            <Document
+                file="https://lucasresume.s3.us-east-2.amazonaws.com/Lucas+SWE+Resume+Template.pdf"
+                onLoadSuccess={onDocumentLoadSuccess}
+            >
+                <Page pageNumber={1} />
+            </Document>
+        </div>
+    );
+}
