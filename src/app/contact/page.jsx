2169e780cbfdd9817baeb2fdc2e06d9eb049f83b
Lucas Yan <lucasyanyikang88@gmail.com>
2025-03-31 16:17:50 +0800

    rough contact form

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -2,14 +2,30 @@ import "../globals.css";
 export default function Contact() {
     return (
         <form action="/api/submit-form" method="POST" className="contact-form">
-            <label>Email</label>
-            <input
-                type="email"
-                name="email"
-                placeholder="Your Email"
-                required
-                style={{ color: "black" }}
-            />
+            <div className="contact-group">
+                <label>Email</label>
+                <input
+                    type="email"
+                    name="email"
+                    placeholder="Your Email"
+                    required
+                    style={{ color: "black" }}
+                />
+            </div>
+            <div className="contact-group">
+                <label>Message</label>
+                <input
+                    type="text"
+                    name="message"
+                    placeholder="Write me a message!"
+                    required
+                    style={{ color: "black" }}
+                />
+            </div>
+
+            <button type="submit" className="button">
+                Send!
+            </button>
         </form>
     );
 }
