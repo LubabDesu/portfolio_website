7dfa5f63425a0c4634bfa5c955f42f13b5d080b8
Lucas Yan <lucasyanyikang88@gmail.com>
2025-03-31 15:35:19 +0800

    contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,3 +1,15 @@
+import "../globals.css";
 export default function Contact() {
-    return <div>Contact page</div>;
+    return (
+        <form action="/api/submit-form" method="POST" className="contact-form">
+            <label>Email</label>
+            <input
+                type="email"
+                name="email"
+                placeholder="Your Email"
+                required
+                style={{ color: "black" }}
+            />
+        </form>
+    );
 }
